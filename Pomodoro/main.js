56526ad21e94d695c9c39f4d2ce90f42dd9db8a1
SenpaiMarques <140733034+SenpaiMarques@users.noreply.github.com>
2023-08-26 00:09:41 +0100

    blur

--- Pomodoro/main.js
+++ Pomodoro/main.js
@@ -161,6 +161,27 @@ document.addEventListener('DOMContentLoaded', () => {
     switchMode('pomodoro');
   });
 
+//------------------ blut-----------//
+
+const handleOnMouseMove = e => {
+    const { currentTarget: target} = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+
+
+}
+
+for(const body of document.querySelectorAll("body")){
+    body.onmousemove = e => handleOnMouseMove(e);
+}
+
+
+
 
 
   // --------------- TODO LIST ---------------//
